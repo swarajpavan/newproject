db74c5a96f114c601d8ea90eefe411038c6ee8b8
swarajpavan <vulshaswarajpavan@gmail.com>
2024-12-19 16:13:45 +0530

    newproject

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { productcomponent } from './shared/components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    productcomponent
   ],
   imports: [
     BrowserModule
